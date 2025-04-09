b029a1ca82bad43ef3fcb18a0fe65cb22c9a62cb
MasterIwan <114767161+MasterIwan@users.noreply.github.com>
2025-04-09 12:59:26 +0200

    I made it work

---
+++ Mobile Sensor Demo/.github/copilot-instructions.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Mobile Sensor Demo
+- Unity version: Unity 2022.3.45f1
+- Active game object:
+  - Name: Plane
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Mobile Sensor Demo/Assets/Balls/Attitude.cs
+++ Mobile Sensor Demo/Assets/Balls/Attitude.cs
@@ -1,16 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Android;
+using Gyroscope = UnityEngine.Gyroscope;
+
 
 
 public class Attitude : MonoBehaviour
 {
+    private Gyroscope gyro;
+    private bool gyroSupported;
 
-    private void Start() {
 
+    void Start()
+    {
+        gyroSupported = SystemInfo.supportsGyroscope;
+
+        if (gyroSupported)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            Debug.Log("Gyroscope is supported and enabled.");
+        }
+        else
+        {
+            Debug.Log("Gyroscope not supported on this device.");
+        }
     }
-    
-    private void Update() {
 
+    void Update()
+    {
+        // Get device rotation as quaternion
+        Quaternion deviceRotation = gyro.attitude;
+
+        // Convert to Unity coordinate space (Android/iOS native to Unity)
+        Quaternion adjustedRotation = new Quaternion(-deviceRotation.x, -deviceRotation.y, deviceRotation.z, deviceRotation.w);
+
+        // Get angles
+        Vector3 eulerAngles = adjustedRotation.eulerAngles;
+
+        float rotateX = eulerAngles.x;
+        float rotateY = eulerAngles.y;
+        float rotateZ = eulerAngles.z;
+        gameObject.transform.rotation = Quaternion.Euler(rotateX, 45, rotateY); 
+        
     }
 }
+
